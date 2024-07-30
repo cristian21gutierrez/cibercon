a5d0a35469d09fcfe20f8cc623ee07a3169bb689
Cristian <95443309+cautan@users.noreply.github.com>
2024-07-30 15:31:55 -0300

    implementamos login-register

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import UserList from './components/UserList';
 import UserDetail from './components/UserDetail';
 import EditUser from './components/EditUser';
-import Home from './components/Home'; // Asegúrate de tener este componente
+import Home from './components/Home';
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Verifica si hay un token en localStorage para determinar si el usuario está autenticado
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/users/:id" element={<UserDetail />} />
-        <Route path="/users/:id/edit" element={<EditUser />} />
-        <Route path="/home" element={<Home />} /> {/* Ruta de inicio */}
+        <Route
+          path="/home"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/users"
+          element={isAuthenticated ? <UserList /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/users/:id"
+          element={isAuthenticated ? <UserDetail /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/users/:id/edit"
+          element={isAuthenticated ? <EditUser /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
